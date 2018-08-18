1b421a48b3cd2fe06396b0448717b97ebff45941
TimurTsokov <tsokov2t1m5@gmail.com>
2018-08-18 16:56:32 +0300

    +finish--kostili

--- js/jq-script.js
+++ js/jq-script.js
@@ -24,10 +24,36 @@
             $(this).parent().addClass('active');
             e.preventDefault();
         });
-        $('#slider').slick({ //слайдер
-            autoplay: true,
+        $('.slider').slick({
             arrows: false,
-            infinite: true
+            dots: true,
+            infinite: false,
+            fade: true,
+            easing: 'ease-in'
         });
+    });
+var map, marker, infoWindow, // Подключение карты
+    center = {
+        lat: 47.1013689,
+        lng: 37.5483022
+    };
 
-	});
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 16
+    });
+
+    marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Hello World!',
+        icon: 'img/Marker.png'
+    });
+    infoWindow = new google.maps.InfoWindow({
+        content: '<h3>info window of Beetroot</h3>'
+    });
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+}
