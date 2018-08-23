4ac719c429ba5bbb843369227c28703a6da91e13
TimurTsokov <tsokov2t1m5@gmail.com>
2018-08-23 09:56:37 +0300

    +scroll bg header

--- js/jq-script.js
+++ js/jq-script.js
@@ -24,10 +24,43 @@
             $(this).parent().addClass('active');
             e.preventDefault();
         });
-        $('#slider').slick({ //слайдер
-            autoplay: true,
+        $('.slider').slick({
             arrows: false,
-            infinite: true
+            dots: true,
+            infinite: false,
+            fade: true,
+            easing: 'ease-in'
         });
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > '900') {
+                $('header').css({"background-color": "rgba(25, 189, 154, 0.7)"});
+            } else if ($(window).scrollTop() < '900') {
+                $('header').css({"background-color": "transparent"});
+            }
+        })
+    });
+var map, marker, infoWindow, // Подключение карты
+    center = {
+        lat: 47.1013689,
+        lng: 37.5483022
+    };
 
-	});
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 16
+    });
+
+    marker = new google.maps.Marker({
+        position: center,
+        map: map,
+        title: 'Hello World!',
+        icon: 'img/Marker.png'
+    });
+    infoWindow = new google.maps.InfoWindow({
+        content: '<h3>info window of Beetroot</h3>'
+    });
+    marker.addListener('click', function () {
+        infoWindow.open(map, marker);
+    });
+}
